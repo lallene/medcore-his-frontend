207c4ca676af160044731da3e30ec0ee763e1212
Lallene Cedric <lallene2016@gmail.com>
2026-07-01 17:51:11 +0200

    - MedCore HIS Frontend - CICD

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import tailwindcss from '@tailwindcss/vite';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 import { defineConfig } from 'vite';
@@ -19,7 +19,11 @@ export default defineConfig({
 				runes: ({ filename }) =>
 					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
 			},
-			adapter: adapter()
+			adapter: adapter({
+				pages: 'build',
+				assets: 'build',
+				fallback: 'index.html'
+			})
 		})
 	]
 });
